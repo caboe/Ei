d3f138411e970bfa251239d03c067c7fe127723e
Your Name <ca_boe@yahoo.de>
2011-02-20 04:26:01 +0100

    recursive

--- js/Ei.js
+++ js/Ei.js
@@ -1,29 +1,35 @@
 var Ei=function(){
 
     var eiArray = new Array();
+    var mainEi = null;
 
     var parseDOM = function(baseEl){
         /*is this final ei*/
         if (!baseEl.find('[ei]').length && $.type(baseEl.attr('ei')) != "undefined"){
             generateEi(0, baseEl);
         }
-        else{
+        else {
             var eiElements = baseEl.find('[ei]');
-            $.each(eiElements, generateEi);
+            var filteredEiElements = new Array();
+            for (var i=0; i<eiElements.length; i++){
+                if ($(eiElements[i]).parent('[ei]').length == 0){
+                    filteredEiElements.push(eiElements[i]);
+                }
+            }
+            $.each(filteredEiElements, generateEi);
         }
-/*
-       var eiElements = (baseEl.attr('ei'))?baseEl:baseEl.find('[ei]');
-        $.each(eiElements, generateEi);
-*/
     };
 
     var generateEi = function(idx, el){
         var config= {};
         config.id = $(el).ei;
         config.container = $(el);
-        eiArray.push(new EiObject(config));
+        config.template = $(el).html().replace(/%7B/g, '{').replace(/%7D/g, "}");
+        mainEi = new EiObject(config);
+        mainEi.start();
+        eiArray.push(mainEi);
     };
 
     $(document).ready(function(){
-            parseDOM($('#a'))});
+            parseDOM($('body'))});
 }();
--- js/EiObject.js
+++ js/EiObject.js
@@ -3,10 +3,31 @@ var EiObject = function(config) {
     var container = config.container;
     var dataUrl = container.attr('dataUrl');
     var mapping = container.attr('mapping').split('.') || '';
-    var template = container.html().replace(/%7B/g, '{').replace(/%7D/g, "}");
+    var template = config.template;
     var data = new Array();
     var self = this;
+    var eiArray = new Array();
+    var eiTemplateArray = new Array();
 
+    var saveSubEiTemplate = function(idx, el){
+        console.log("saving sub ei: " + el);
+        var config= {};
+        config.id = $(el).attr('ei');
+        config.container = $(el);
+        config.template = $(el).html().replace(/%7B/g, '{').replace(/%7D/g, "}");
+        eiTemplateArray.push(config);
+    };
+
+    var processSubEi = function(idx, el){
+        console.log("processSubEi: " + el);
+        var config= {};
+        config.id = $(el).ei;
+        config.container = $(el);
+        config.template = $(el).html().replace(/%7B/g, '{').replace(/%7D/g, "}");
+        var mainEi = new EiObject(config);
+        mainEi.start();
+        eiArray.push(mainEi);
+    };
 
     var initData = function(rawData){
        for (var i=0; i<mapping.length; i++){
@@ -19,7 +40,8 @@ var EiObject = function(config) {
         var jsonObj = {};
         var result = '';
         var tmpl = '';
-        for (var i=0; i<data.length; i++){
+        var i = 0;
+        for (i=0; i<data.length; i++){
             jsonObj = data[i];
             tmpl = template;
             for (var prop in jsonObj){
@@ -29,6 +51,13 @@ var EiObject = function(config) {
             result += tmpl;
         }
         container.html(result);
+        //then replace SubEis
+        var subEis = new Array();
+        for (i=0; i<eiTemplateArray.length; i++){
+            subEis = container.find('[ei=' + eiTemplateArray[i].id + ']');
+            subEis.html(eiTemplateArray[i].template);
+            $.each(subEis, processSubEi);
+        }
     };
 
     var callback = function() {
@@ -38,5 +67,17 @@ var EiObject = function(config) {
         }
     };
 
-    $.getJSON(dataUrl,callback());
+    //show Loading
+    var loading = config.loading || function(){
+       container.html("loading...");
+    };
+
+    //has subEis?
+    var subEis = container.find('[ei]');
+    $.each(subEis, saveSubEiTemplate);
+    loading();
+
+    this.start = function(){
+        $.getJSON(dataUrl,callback());
+    }
 };
